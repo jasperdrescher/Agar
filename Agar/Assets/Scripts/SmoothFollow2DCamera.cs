b1b04bce1ed1db239fcff219abfa7411fa07fceb
JasperDre <jasperdre@gmail.com>
2018-01-01 21:27:33 +0100

    [ADDED] Smooth camera

---
+++ Agar/Assets/Scripts/SmoothFollow2DCamera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothFollow2DCamera : MonoBehaviour
+{
+    [Header("Camera settings")]
+    [Tooltip("Reference to the target GameObject")]
+    public Transform target;
+    [Tooltip("Current relative offset to the target")]
+    public Vector3 offset;
+    [Tooltip("Multiplier for the movement speed")]
+    [Range(0f, 5f)]
+    public float smoothSpeed = 0.125f;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate ()
+    {
+        Vector3 position = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, position, smoothSpeed);
+        transform.position = smoothedPosition;
+        transform.LookAt(target);
+	}
+}
