e5f9e8ea49245fb124b3914fc9fa98fc27fdb042
JasperDre <jasperdre@gmail.com>
2018-01-03 21:08:30 +0100

    [CHANGED] SplitMassController

--- Agar/Assets/Scripts/SplitMassController.cs
+++ Agar/Assets/Scripts/SplitMassController.cs
@@ -4,17 +4,24 @@ using System.Collections.Generic;
 
 public class SplitMassController : MonoBehaviour
 {
-    public float movementSpeed = 50.0f;
+    public GameObject splitMass;
 
+    public float movementSpeed = 50.0f;
+    public float massSplitMultiplier = 0.5f;
+    public float increase = 0.05f;
     public Vector2 movement;
     public Vector2 mouseDistance;
 
     private Rigidbody2D rigidBody2D;
+    private GameObject gameManager;
+    private GameManager managerScript;
 
     // Use this for initialization
-    void Start ()
+    void Start()
     {
         rigidBody2D = GetComponent<Rigidbody2D>();
+        gameManager = GameObject.Find("GameManager");
+        managerScript = gameManager.GetComponent<GameManager>();
     }
 
     // FixedUpdate is used for physics
@@ -25,5 +32,34 @@ public class SplitMassController : MonoBehaviour
         movement.x = Input.GetAxis("Horizontal") + mouseDistance.x;
         movement.y = Input.GetAxis("Vertical") + mouseDistance.y;
         rigidBody2D.velocity = movement * movementSpeed * Time.deltaTime;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            if (transform.localScale.x * massSplitMultiplier >= 1.0f)
+            {
+                transform.localScale = transform.localScale * massSplitMultiplier;
+                GameObject newSplitMass = Instantiate(splitMass, transform.position + new Vector3(-0.6f, 0.8f, 0), transform.rotation) as GameObject;
+                newSplitMass.transform.localScale = transform.localScale;
+            }
+            else
+            {
+                managerScript.PrintToConsole("Can't split mass!", "log");
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Food")
+        {
+            managerScript.PrintToConsole("Ate food", "log");
+            transform.localScale += new Vector3(increase, increase, 0);
+            other.GetComponent<Food>().RemoveObject();
+            managerScript.UpdateScore(10);
+        }
     }
 }
