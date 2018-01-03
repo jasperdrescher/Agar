58b9ba600dafb0dff6df4c813e4c0814f210c517
JasperDre <jasperdre@gmail.com>
2018-01-03 18:20:28 +0100

    [ADDED] AudioManager and assets

---
+++ Agar/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+    public string name;
+    public AudioClip clip;
+    [Range(0f, 1f)]
+    public float volume = 0.7f;
+    [Range(0.5f, 1.5f)]
+    public float pitch = 1f;
+    [Range(0f, 0.5f)]
+    public float randomVolume = 0.1f;
+    [Range(0f, 0.5f)]
+    public float randomPitch = 0.1f;
+    public bool loop = false;
+
+    private AudioSource source;
+
+    public void SetSource(AudioSource _source)
+    {
+        source = _source;
+        source.clip = clip;
+        source.loop = loop;
+    }
+
+    public void Play()
+    {
+        source.volume = volume * (1 + Random.Range(-randomVolume / 2f, randomVolume / 2f));
+        source.pitch = pitch * (1 + Random.Range(-randomPitch / 2f, randomPitch / 2f));
+        source.Play();
+    }
+
+    public void Stop()
+    {
+        source.Stop();
+    }
+}
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+
+    [SerializeField]
+    Sound[] sounds;
+
+    private GameObject gameManager;
+    private GameManager managerScript;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            if (instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+        else
+        {
+            instance = this;
+            //DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager");
+        managerScript = gameManager.GetComponent<GameManager>();
+
+        for (int i = 0; i < sounds.Length; i++)
+        {
+            GameObject _go = new GameObject("Sound_" + i + "_" + sounds[i].name);
+            _go.transform.SetParent(this.transform);
+            sounds[i].SetSource(_go.AddComponent<AudioSource>());
+        }
+
+        PlaySound("BackgroundMusic");
+    }
+
+    public void PlaySound(string _name)
+    {
+        for (int i = 0; i < sounds.Length; i++)
+        {
+            if (sounds[i].name == _name)
+            {
+                sounds[i].Play();
+                return;
+            }
+        }
+
+        managerScript.PrintToConsole("Sound not found in list, " + _name, "error");
+    }
+
+    public void StopSound(string _name)
+    {
+        for (int i = 0; i < sounds.Length; i++)
+        {
+            if (sounds[i].name == _name)
+            {
+                sounds[i].Stop();
+                return;
+            }
+        }
+
+        managerScript.PrintToConsole("Sound not found in list, " + _name, "error");
+    }
+}
