44ca71abd2567ec97cce15afc6389b0b26fef6b0
JasperDre <jasperdre@gmail.com>
2018-01-03 13:01:40 +0100

    [ADDED] Scene fading

---
+++ Agar/Assets/Scripts/ImageFader.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageFader : MonoBehaviour
+{
+    public float fadeSpeed = 0.8f;
+    public enum FadeDirection
+    {
+        In, //Alpha = 1
+        Out // Alpha = 0
+    }
+
+    private Image fadeOutUIImage;
+
+    void Start()
+    {
+        fadeOutUIImage = GetComponent<Image>();
+        StartCoroutine(Fade(FadeDirection.Out));
+    }
+
+    private IEnumerator Fade(FadeDirection fadeDirection)
+    {
+        float alpha = (fadeDirection == FadeDirection.Out) ? 1 : 0;
+        float fadeEndValue = (fadeDirection == FadeDirection.Out) ? 0 : 1;
+        if (fadeDirection == FadeDirection.Out)
+        {
+            while (alpha >= fadeEndValue)
+            {
+                SetColorImage(ref alpha, fadeDirection);
+                yield return null;
+            }
+            fadeOutUIImage.enabled = false;
+        }
+        else
+        {
+            fadeOutUIImage.enabled = true;
+            while (alpha <= fadeEndValue)
+            {
+                SetColorImage(ref alpha, fadeDirection);
+                yield return null;
+            }
+        }
+    }
+
+    private void SetColorImage(ref float alpha, FadeDirection fadeDirection)
+    {
+        fadeOutUIImage.color = new Color(fadeOutUIImage.color.r, fadeOutUIImage.color.g, fadeOutUIImage.color.b, alpha);
+        alpha += Time.deltaTime * (1.0f / fadeSpeed) * ((fadeDirection == FadeDirection.Out) ? -1 : 1);
+    }
+}
--- Agar/Assets/Scripts/PlayerController.cs
+++ Agar/Assets/Scripts/PlayerController.cs
@@ -26,11 +26,11 @@ public class PlayerController : MonoBehaviour
     // FixedUpdate is used for physics
     private void FixedUpdate()
     {
-        mouseDistance.x = (Input.mousePosition.x - Camera.main.WorldToScreenPoint(gameObject.transform.position).x) * 0.001f;
-        mouseDistance.y = (Input.mousePosition.y - Camera.main.WorldToScreenPoint(gameObject.transform.position).y) * 0.001f;
+        mouseDistance.x = (Input.mousePosition.x - Camera.main.WorldToScreenPoint(gameObject.transform.position).x) * 0.005f;
+        mouseDistance.y = (Input.mousePosition.y - Camera.main.WorldToScreenPoint(gameObject.transform.position).y) * 0.005f;
         movement.x = Input.GetAxis("Horizontal") + mouseDistance.x;
         movement.y = Input.GetAxis("Vertical") + mouseDistance.y;
-        rigidBody2D.AddForce(movement * movementSpeed * Time.deltaTime);
+        rigidBody2D.velocity = movement * movementSpeed * Time.deltaTime;
     }
 
     // Update is called once per frame
