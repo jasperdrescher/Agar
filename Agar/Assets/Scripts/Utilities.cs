e92c11172b47042c31900af7aa4be6b4430a42b4
JasperDre <jasperdre@gmail.com>
2018-01-04 19:25:21 +0100

    [ADDED] Utilities class

---
+++ Agar/Assets/Scripts/Utilities.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utilities : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    /// <summary>
+    /// Prints the given message in the console with extra information.
+    /// </summary>
+    public void Print(string message, string severity)
+    {
+        switch (severity)
+        {
+            case "log":
+                Debug.Log(gameObject.name + ": " + message + " @ " + Time.time + " seconds.");
+                break;
+            case "event":
+                Debug.Log("<color=orange>(Event) </color>" + gameObject.name + ": " + message + " @ " + Time.time + " seconds");
+                break;
+            case "warning":
+                Debug.LogWarning(gameObject.name + ": " + message + " @ " + Time.time + " seconds");
+                break;
+            case "error":
+                Debug.LogError(gameObject.name + ": " + message + " @ " + Time.time + " seconds");
+                break;
+
+            default:
+                Debug.Log(gameObject.name + ": " + message + " @ " + Time.time + " seconds");
+                break;
+        }
+    }
+}
