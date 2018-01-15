f6390da89a5ba4bc1126785cec476a81ec53e416
JasperDre <jasperdre@gmail.com>
2018-01-15 16:17:54 +0100

    [ADDED] FPS counter

--- Agar/Assets/Scripts/Managers/MenuManager.cs
+++ Agar/Assets/Scripts/Managers/MenuManager.cs
@@ -7,6 +7,7 @@ using System.Text;
 public class MenuManager : Utilities
 {
     public GameObject pausePanel;
+    public bool showFrameRate = true;
     public Text scoreText;
     public Text highscoreText;
     public Text elapsedTimeText;
@@ -89,6 +90,25 @@ public class MenuManager : Utilities
         else
         {
             canvas.enabled = true;
+        }
+    }
+
+    void OnGUI()
+    {
+        if (showFrameRate)
+        {
+            int w = Screen.width, h = Screen.height;
+
+            GUIStyle style = new GUIStyle();
+
+            Rect rect = new Rect(0, 0, w, h * 2 / 100);
+            style.alignment = TextAnchor.UpperLeft;
+            style.fontSize = h * 2 / 100;
+            style.normal.textColor = new Color(0.0f, 0.0f, 0.5f, 1.0f);
+            float msec = Time.deltaTime * 1000.0f;
+            float fps = 1.0f / Time.deltaTime;
+            string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
+            GUI.Label(rect, text, style);
         }
     }
 }
